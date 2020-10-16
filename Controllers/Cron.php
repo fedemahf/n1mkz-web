e0a9d020f12af6b41f52160550a00f94eff9e857
Autorojo <autogris@gmail.com>
2020-10-16 14:29:43 -0300

    cron test

--- Controllers/Cron.php
+++ Controllers/Cron.php
@@ -1,5 +1,7 @@
 <?php namespace App\Controllers;
 
+use \App\Controllers\Vip;
+
 class Cron extends BaseController
 {
 	/**
@@ -18,8 +20,8 @@ class Cron extends BaseController
 
 	public function index()
 	{
-		$vipController = new \App\Controllers\Vip();
-		$vipController->desactivarVip(16);
+		// $vipController = new \App\Controllers\Vip();
+		$this->desactivarVip(16);
 		echo "ok" . PHP_EOL;
 	}
 }

22778ec54e32da1409435d165929b2b981e0d800
Autorojo <autogris@gmail.com>
2020-12-07 01:48:45 -0300

    Añadidos tutoriales de KZ en la portada

---
+++ Views/portada/tutoriales.php
@@ -0,0 +1,19 @@
+				<div class="card mt-3">
+					<div class="card-body">
+						<h5 class="card-title mb-0">Tutoriales de KZ</h5>
+						<div class="container">
+							<div class="row">
+								<div class="col-lg-6 mt-3">
+									<div class="embed-responsive embed-responsive-16by9">
+										<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/kqPFL90Gsls?rel=0" allowfullscreen=""></iframe>
+									</div>
+								</div>
+								<div class="col-lg-6 mt-3">
+									<div class="embed-responsive embed-responsive-16by9">
+										<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/zUJoKvfMG94?rel=0" allowfullscreen=""></iframe>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
--- Views/templates/portada.php
+++ Views/templates/portada.php
@@ -4,6 +4,7 @@ echo view('portada/header');
 echo view('portada/contenido', $dataContenido);
 echo view('portada/vip', $dataVip);
 echo view('portada/sourcequery');
+echo view('portada/tutoriales');
 echo view('portada/footer');
 
 ?>

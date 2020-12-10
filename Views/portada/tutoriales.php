e29145c67c2042c0aab15948e79d07e350a9a5bd
Autorojo <autogris@gmail.com>
2020-12-10 12:58:04 -0300

    Vídeo añadido en apartado de tutoriales

--- Views/portada/tutoriales.php
+++ Views/portada/tutoriales.php
@@ -17,3 +17,17 @@
 						</div>
 					</div>
 				</div>
+				<div class="card mt-3">
+					<div class="card-body">
+						<h5 class="card-title mb-0">Tutorial de preparación de mate</h5>
+						<div class="container">
+							<div class="row">
+								<div class="col mt-3">
+									<div class="embed-responsive embed-responsive-16by9">
+										<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/5Hm8trFizMo?rel=0" allowfullscreen=""></iframe>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>

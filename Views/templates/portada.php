8e3d129329e210eedf9c5cc7063c9dd04be2b468
Autorojo <autogris@gmail.com>
2021-03-01 10:20:41 -0300

    Views vip y sourcequery ocultos

--- Views/templates/portada.php
+++ Views/templates/portada.php
@@ -3,8 +3,8 @@
 echo view('portada/header');
 echo view('portada/contenido', $dataContenido);
 if(ENVIRONMENT == 'development') echo view('portada/sorteo', $dataSorteo);
-echo view('portada/vip', $dataVip);
-echo view('portada/sourcequery');
+// echo view('portada/vip', $dataVip);
+// echo view('portada/sourcequery');
 echo view('portada/tutoriales');
 echo view('portada/footer');
 
